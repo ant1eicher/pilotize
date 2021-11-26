0228e27f2282efc16a28045c2b85b52a86ee29ea
Anton Eicher <anton.eicher@stedi.com>
2021-11-26 20:51:34 +0200

    feat: pilotize 0.1 - convert mph to kts

---
+++ src/pilotize.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Pilotize
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Grease/Tampermonkey script to standardise website units like mph to kts for pilots
+// @author       LM
+// @match        https://www.vansaircraft.com/*
+// @match        https://avcom.co.za/*
+// @match        https://vansairforce.net/*
+// @match        https://www.zenithair.net/*
+// @match        https://savannah-aircraft.co.za/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+
+function MphToKts(child) {
+    child.innerHTML = child.innerHTML.replace(/[0-9]+[ ]?(mph|MPH)/g, (match) => {
+        const num = parseFloat(match.match(/[0-9]+/)[0]);
+        const kts = Math.round(num * 0.868976);
+        return `${kts} kts`;
+    });
+}
+
+(function () {
+    'use strict';
+
+    for (const child of document.body.children) {
+        MphToKts(child);
+    }
+})();
